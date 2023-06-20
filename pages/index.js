7b02c60a4827a2fe7f0fb4f38b4ab03d145d6851
Zahra Khavari <Khavari.khu@gmail.com>
2023-06-21 01:23:22 +0430

    fix routes

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ const App =()=>{
                 <Link href={"/users"}><h3><li> users (sg)</li></h3></Link>
                 <Link href={"/albums"}><h3><li> albums (ssr) </li></h3></Link>
                 <Link href={"/comments"}> <h3><li> users with comments (csr & sg) </li></h3></Link>
-                <Link href={"/user"}> <h3><li> test </li></h3></Link>
-                <Link href={"/employe"}> <h3><li> employe </li></h3></Link>
-                <Link href={"/serverside"}> <h3><li> serverside test </li></h3></Link>
+                <Link href={"/user"}> <h3><li> display users with details </li></h3></Link>
+                <Link href={"/employe"}> <h3><li> add user </li></h3></Link>
+                <Link href={"/articles"}> <h3><li> articles </li></h3></Link>
                 
             </ul>
         </div>
